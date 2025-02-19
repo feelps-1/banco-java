5bbd0e457d9a091d3f5837703e9655064f54f1c1
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2025-02-19 10:55:22 -0300

    first commit

---
+++ src/test/java/me/decola/tech/banco/BancoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.decola.tech.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
